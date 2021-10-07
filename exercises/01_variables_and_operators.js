8433a3cbb0cb4aceae0cf4fb62f61856efe13133
nicolasvaudagna <nico.vaudagna14@gmail.com>
2021-10-07 17:31:38 -0300

    variables and operators tasks completed

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,14 @@
+var x = 2
+var y = 3
+var z = (x + y)
+console.log("x + y =" + z)
+
+var first_name = "Nicolas"
+var lastname = "Vaudagna"
+var full_name = (first_name + lastname)
+console.log("My name is " + full_name)
+
+var city = "Rosario"
+var country = "Argentina"
+var letters = (city.length + country.length)
+console.log ("the words have " + letters + " letters")
