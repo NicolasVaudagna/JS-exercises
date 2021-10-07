55cdd1585f7a16208c19cc78afc9410b4853feca
nicolasvaudagna <nico.vaudagna14@gmail.com>
2021-10-07 19:05:29 -0300

    strings tasks completed

---
+++ exercises/02_strings.js
@@ -0,0 +1,25 @@
+var brand = "playstation"
+console.log (brand.toUpperCase())
+
+var car = "Mercedes Benz"
+var merce = car.substring(0,5)
+console.log (merce)
+
+var profession = "entrepreneur"
+var eur = profession.substring(9, 13)
+console.log (eur)
+
+var drink = "Sex On The Beach"
+var drink1 = (drink.substring(0, 1))
+var drink2 = (drink.substring(1, 16))
+console.log (drink1.toUpperCase() + drink2.toLowerCase())
+
+var movie = "Harry Potter"
+var movie1 = movie.indexOf(" ")
+console.log (movie1)
+
+// var degree = "international relations"
+// var degree1 = degree.substring(1)
+// var degree2 = degree.substring(2)
+// console.log (degree1.toUpperCase(1) + degree2.toUpperCase(1))
+// No me sale
