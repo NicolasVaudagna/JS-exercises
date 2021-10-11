3961e543d19b52fea22c70b68d49d40317ca3ea8
nicolasvaudagna <nico.vaudagna14@gmail.com>
2021-10-11 11:21:30 -0300

    for tasks completed

--- exercises/03_arrays.js
+++ exercises/03_arrays.js
@@ -18,7 +18,6 @@ console.log(months)
 
 console.log(months.join("-"))
 
-
 // Creo una variable nueva con los meses ordenados porque estaban en orden alfabetico
 var months1 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
 "Septiembre", "Octubre", "Noviembre", "Diciembre"]
---
+++ exercises/05_for.js
@@ -0,0 +1,32 @@
+var days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+for (let contador = 0; contador < days.length; contador++) {
+    console.log(days[contador]);
+}
+
+for (let index = 0; index < days.length; index++) {
+    var days1 = days[index].substring(0,1)
+    days1 = days1.toUpperCase()
+
+    var days2 = days[index].substring(1)
+    alert(days1+days2)
+}
+
+var sentence = ""
+for (let index = 0; index < days.length; index++) {
+    sentence = sentence + days[index] 
+    
+}
+alert(sentence)
+
+var empty = [""]
+for (let index = 0; index < 10; index++) {
+    empty.push(1)
+}
+console.log (empty)
+// REVISAR
+
+
+
+
+
+
