566e90c55850a9e349604cbb97f78e76f88e22c3
nicolasvaudagna <nico.vaudagna14@gmail.com>
2021-10-11 21:40:58 -0300

    Function tasks A & B completed

--- exercises/06_functions.js
+++ exercises/06_functions.js
@@ -1,6 +1,11 @@
-function Accion1 (numero) {
-    console.log (numero + 5)
+function suma (a, b) {
+    return a + b;
+}
+var resultado = suma(10, 15)
+console.log(resultado)
+
+if (typeof (a) === "number")
+{
+    alert("ALERT")
 }
 
-Accion1 (30)
-Accion1 (40)
